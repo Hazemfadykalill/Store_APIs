5f9d08bb00afab8f59b9ab97eb3102cae19ece80
Hazem Fady <hazemfady123@gmail.com>
2024-10-02 09:29:59 +0300

    Finished All Repositories and interface Generic

---
+++ Store.HazemFady.Repository/Repositories/GenericRepository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.HazemFady.Core.Entities;
+using Store.HazemFady.Core.Repositories.Contract;
+using Store.HazemFady.Repository.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.HazemFady.Repository.Repositories
+{
+    public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private readonly StoreDbContext storeDb;
+
+        public GenericRepository(StoreDbContext storeDb)
+        {
+            this.storeDb = storeDb;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            if (typeof(TEntity) == typeof(Product))
+            {
+                return (IEnumerable<TEntity>)await storeDb.Products.Include(B => B.Brand).Include(T => T.Type).ToListAsync();
+
+            }
+            return await storeDb.Set<TEntity>().ToListAsync();
+        }
+
+
+        public async Task<TEntity> GetAsync(TKey id)
+        {
+            if (typeof(TEntity) == typeof(Product))
+            {
+                return await storeDb.Products.Include(B => B.Brand).Include(T => T.Type).FirstOrDefaultAsync(P => P.Id == id as int?) as TEntity;
+
+            }
+            return await storeDb.Set<TEntity>().FindAsync(id);
+        }
+        public async Task AddAsync(TEntity entity)
+        {
+            await storeDb.AddAsync(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            storeDb.Update(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            storeDb.Remove(entity);
+
+        }
+
+
+
+
+
+    }
+}
