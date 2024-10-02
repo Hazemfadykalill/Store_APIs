a6dc4f2895093a844e21cbdaa3bd9979af497c7b
Hazem Fady <hazemfady123@gmail.com>
2024-10-02 05:01:28 +0300

    ProductEntities

---
+++ Store.HazemFady.Core/Entities/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿// Ignore Spelling: Fady
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.HazemFady.Core.Entities
+{
+    public class BaseEntity<TKey>
+    {
+        public TKey Id { get; set; }
+        public DateTime CreationAt { get; set; } = DateTime.UtcNow;
+
+
+    }
+}
