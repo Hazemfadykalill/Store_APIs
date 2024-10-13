e006141f138da25fd93d26d2940cd20dc06e3afe
Hazem Fady <hazemfady123@gmail.com>
2024-10-13 11:48:05 +0300

    Cache Service

---
+++ Store.HazemFady.Core/Services.Contract/ICashService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.HazemFady.Core.Services.Contract
+{
+    public  interface ICashService
+    {
+
+        //func To Get Data From Database To Set In Cache
+        Task SetInCacheAsync(string key, object Response,TimeSpan ExpireTime);
+
+
+        //func To Get Data From Cache
+        Task<string> GetCacheKeyAsync(string key);
+    }
+}
